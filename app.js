0727acb720fd4307437bac56612c6bf51f8ef485
Gahee Kim <gaheekimedu@gmail.com>
2023-08-26 16:10:28 -0700

    Add active nav link indicator

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('.navbar__menu a').
+forEach (link => {
+    if (link.href.includes(`${activePage}`)) {
+        link.classList.add('active');
+        console.log(`${activePage}`)
+    }
+})
